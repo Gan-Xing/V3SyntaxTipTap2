430f4612fad63952b6004a0a2eec6794210aebf2
Gan-Xing <599153574@qq.com>
2023-09-26 12:05:03 +0800

    Initial commit

---
+++ src/components/TiptapEditor.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <EditorContent :editor="editor" />
+  </div>
+</template>
+
+<script>
+import { ref, onMounted, onBeforeUnmount } from '@vue/composition-api';
+import { Editor, EditorContent } from '@tiptap/vue-2';
+import StarterKit from '@tiptap/starter-kit';
+
+export default {
+  name: 'TiptapEditor',
+  components: {
+    EditorContent
+  },
+  setup() {
+    const editor = ref(null);
+
+    onMounted(() => {
+      editor.value = new Editor({
+        extensions: [
+          StarterKit
+        ],
+        content: '<p>Hello World! 🌍</p>',
+      });
+    });
+
+    onBeforeUnmount(() => {
+      if (editor.value) {
+        editor.value.destroy();
+      }
+    });
+
+    return {
+      editor
+    };
+  }
+}
+</script>
